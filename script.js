9b522c6acd07da0b07cacd17201300fc46241045
Jon Snow <dahiyaharsh2005@gmail.com>
2025-03-14 00:09:06 +0530

    add

--- script.js
+++ script.js
@@ -1,22 +1,62 @@
-const container= document.getElementById("container");
+const container= document.getElementById("container"); 
+function Node(i,j,row,size){
+    const node= document.createElement("div")
 
-createGrid() {
-    const grid = [];
-    for (let row = 0; row < 20; row++) {
-      const currentRow = [];
-      for (let col = 0; col < 50; col++) {
-        currentRow.push({
-          row,
-          col,
-          isStart: row === start_row && col === start_col,
-          isFinish: row === fin_row && col === fin_col,
-          isWall: false,
-          distance: Infinity,
-          isVisited: false,
-          previousNode: null,
-        });
-      }
-      grid.push(currentRow);
+    node.classList.add("nodes");
+    node.id=`${i}-${j}`;
+    const nodeSize = Math.floor(Math.min(window.innerWidth, window.innerHeight) / size) -2;
+ // Adjust for borders
+    node.style.width = `${nodeSize}px`;
+    node.style.height = `${nodeSize}px`;    
+    row.appendChild(node);
+    
+}
+function sketchpad(size){
+    for (let i = 0; i < size; i++) {
+        const row=document.createElement("div");
+        row.classList.add("row");
+        for (let j = 0; j < size; j++) {
+            Node(i,j,row,size);
+        }
+        container.appendChild(row);
     }
-    return grid;
-  }
+}
+function removes(){
+    container.innerHTML="";
+}
+const clear= document.querySelector("#clear");
+
+clear.onclick =() =>{
+    const usednodes= document.getElementsByClassName("used-node");
+    Array.from(usednodes).forEach(
+        used =>{
+            used.classList.remove("used-node");
+        }
+    )
+}
+const start= document.querySelector("#start");
+
+start.onclick=()=>{
+    const size=prompt("enter size");
+    removes();
+    sketchpad(size);
+    const draw= document.querySelectorAll(".nodes");
+
+    draw.forEach(
+    node =>{
+        node.addEventListener("mouseover", ()=>{
+            node.style.backgroundColor= getRandomColor();
+
+        })
+    }
+)
+}
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
